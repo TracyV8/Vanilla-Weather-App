f29cf14db602a0b2262e8767f0c85b60f58ea372
TracyV8 <123339740+TracyV8@users.noreply.github.com>
2023-02-27 13:08:21 +0800

    Linked icon to API data

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   let days = [
     "Sunday",
     "Monday",
@@ -33,6 +39,11 @@ function displayTemperature(response) {
   );
   document.querySelector("#date").innerHTML = formatDate(
     response.data.time * 1000
+  );
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
 }
 let city = "Perth";
